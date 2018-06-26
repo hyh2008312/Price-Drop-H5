ac1ff17fa33d891d830d37ffdb1c2cbf0cb66d19
zhoushini@163.com <zhoucc88>
2018-06-26 09:36:05 +0800

    bug修复 和文字调整

--- src/app/drops/user-share/user-share.component.ts
+++ src/app/drops/user-share/user-share.component.ts
@@ -20,7 +20,7 @@ import {CutPriceDialogComponent} from '../cut-price-dialog/cut-price-dialog.comp
 
 export class UserShareComponent implements OnInit {
 
-  showLoading: boolean = false;
+  showLoading = false;
   loadingValue: any = 0;
   color = 'Accent';
 
@@ -30,7 +30,6 @@ export class UserShareComponent implements OnInit {
   cutid: any;
   ownerimg: any;
   imgsrc: any;
-  currentPrice: any;
   lowestPrice: any;
   iconpercentage: any;
   percentage: any;
@@ -38,14 +37,16 @@ export class UserShareComponent implements OnInit {
   amin: any;
   asecond: any;
   title: any;
+  currentPrice: any;
   salePrice: any;
+  topPrice: any;
   user: any;
   cutStatus: any;
   friendCuts: any;
   cutamount: any;
   cancut: any;
 
-  isLogin: boolean = false;
+  isLogin = false;
 
   constructor(
     private router: Router,
@@ -59,7 +60,7 @@ export class UserShareComponent implements OnInit {
     public dialog: MatDialog,
   ) {
     this.sub = this.guardLinkService.routerLink.subscribe((data) => {
-      if(data) {
+      if (data) {
         this.loginLink = data;
       }
     });
@@ -82,7 +83,7 @@ export class UserShareComponent implements OnInit {
     self.dropsService.getCutDetail(self.cutid).then((res) => {
 
       console.log(res)
-      let data = {
+      const data = {
         description: res.title,
         title: 'Come help me drop the price before it sells out!',
         shareImage: res.mainImage,
@@ -103,13 +104,12 @@ export class UserShareComponent implements OnInit {
       this.cutStatus = res.cutStatus
       this.friendCuts = res.friendCuts
       this.user = res.user
-
-
-      let nowtime = new Date
-      let tmp = nowtime.getTime()
-      let restmp = res.endTimestamp
+      this.topPrice = (this.salePrice - this.currentPrice).toString().substring(0,(this.salePrice - this.currentPrice).toString().indexOf(".") + 3)
+      const nowtime = new Date
+      const tmp = nowtime.getTime()
+      const restmp = res.endTimestamp
       console.log(restmp - nowtime.getTime())
-      if (this.cutStatus == 'progressing'){
+      if (this.cutStatus == 'progressing') {
         this.editTime(res.endTimestamp)
       } else {
         this.ahour = '00'
@@ -131,33 +131,33 @@ export class UserShareComponent implements OnInit {
   // }
   editTime(time) {
     // const tmp = 1527753479
-    let self = this
+    const self = this
     self.ngZone.runOutsideAngular(() => {
       setInterval(() => {
-        let nowtime = new Date
+        const nowtime = new Date
         const total =  time - nowtime.getTime() / 1000
 
         const day = Math.floor(total / ( 24 * 60 * 60))//整天
 
         const afterDay = total - day * 24 * 60 * 60;
-        self.ahour = Math.floor(afterDay / ( 60 * 60 ));//小时
-        if( self.ahour < 10){
-          self.ahour = '0' +self.ahour
+        self.ahour = Math.floor(afterDay / ( 60 * 60 )); //小时
+        if ( self.ahour < 10) {
+          self.ahour = '0' + self.ahour
         }
-        const afterHour = total - day * 24 * 60 * 60 - self.ahour * 60* 60;
-        self.amin = Math.floor(afterHour / 60);//分钟
-        if( self.amin < 10){
-          self.amin = '0' +self.amin
+        const afterHour = total - day * 24 * 60 * 60 - self.ahour * 60 * 60;
+        self.amin = Math.floor(afterHour / 60); //分钟
+        if ( self.amin < 10) {
+          self.amin = '0' + self.amin
         }
-        const afterMin = total - day * 24 * 60 * 60 - self.ahour * 60 * 60 - self.amin* 60;
+        const afterMin = total - day * 24 * 60 * 60 - self.ahour * 60 * 60 - self.amin * 60;
 
         self.asecond = Math.floor(afterMin)//秒
-        if( self.asecond < 10){
+        if ( self.asecond < 10) {
           self.asecond = '0' + self.asecond
         }
 
         self.changeDetectorRef.detectChanges();
-      },1000)
+      }, 1000)
     })
     // alert(time / (24 * 3600 * 1000))
   }
@@ -188,20 +188,20 @@ export class UserShareComponent implements OnInit {
 
   openCutPrice(price: any) {
     console.log(price)
-    let dialogRef = this.dialog.open(CutPriceDialogComponent, {
+    const dialogRef = this.dialog.open(CutPriceDialogComponent, {
       data: {
         price
       }
     });
 
-    let self = this;
+    const self = this;
   }
   openFaq() {
-    let dialogRef = this.dialog.open(FaqDialogComponent, {
+    const dialogRef = this.dialog.open(FaqDialogComponent, {
       data: {}
     });
 
-    let self = this;
+    const self = this;
 
     dialogRef.afterClosed().subscribe(result => {
     });
@@ -233,7 +233,7 @@ export class UserShareComponent implements OnInit {
   }
 
   private load() {
-    if(this.loadingValue < 90) {
+    if (this.loadingValue < 90) {
       this.loadingValue++;
     } else {
       return;
