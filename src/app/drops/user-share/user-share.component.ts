4a2e58c26cf57e1fc922bcd68a99c13cc217adf7
hyh2008312 <hyh2008312@163.com>
2019-02-20 14:06:51 +0800

    [Drops]:[DropsDetail]:correct the drops detail

--- src/app/drops/user-share/user-share.component.ts
+++ src/app/drops/user-share/user-share.component.ts
@@ -2,62 +2,36 @@ import {Component, OnInit, ChangeDetectorRef, NgZone, OnDestroy} from '@angular/
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 
-// import { LoginService } from '../user-share.service';
-// import { AuthenticationService } from '../../shared/services/authentication/authentication.service';
 import { DropsService } from '../drops.service';
 import { GuardLinkService } from '../../shared/services/guard-link/guard-link.service';
-// import { UserService } from '../../shared/services/user/user.service';
-
-// import {FaqDialogComponent} from '../faq-dialog/faq-dialog.component';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-drops-detail',
   templateUrl: './user-share.component.html',
   styleUrls: ['./_user-share.scss']
 })
 
 export class UserShareComponent implements OnInit, OnDestroy {
 
-  showLoading = false;
   loadingValue: any = 0;
   color = 'Accent';
 
-  loginLink: any = false;
-
   sub: any;
   cutid: any;
-  inLogin: any;
-  ownerimg: any;
   imgsrc: any;
   someGoodsList: any;
   lowestPrice: any;
-  iconpercentage: any;
   percentage: any;
-  wordPercentage: any;
-  wordPercentageNum: any;
   ahour: any;
   amin: any;
   asecond: any;
   title: any;
   currentPrice: any;
   salePrice: any;
-  topPrice: any;
   user: any;
-  cutStatus: 'progressing';
-  friendCuts: any;
-  cutamount: any = 111;
-  cancut: any;
-  faqStauts: any = false;
-  cutPriceStauts: any = false;
-  userArr: any;
-  hasfriends: any;
-  aboutProduct: any;
-  priceOff: any;
   sUserAgent: any;
   dropObj: any = {};
   timer: any;
-
-  sub1: any;
 
   constructor(
     private router: Router,
@@ -89,17 +63,17 @@ export class UserShareComponent implements OnInit, OnDestroy {
         description: '#PriceDrop - Go to page & help me drop the price of ' + res.title + ' to Rs.' + res.lowestPrice + ' ! You can also get it for FREE.',
         title: 'Help Me Drop The Price to Rs. 0!',
         shareImage: res.mainImage,
-      }
-      this.dropObj = res
-      this.imgsrc = res.avatar
+      };
+      this.dropObj = res;
+      this.imgsrc = res.avatar;
       this.percentage = Math.ceil(((5 - res.friendsDrop.length) / 5 ) * 100 ) + '%';
       // this.percentage = '0%';
       if (this.dropObj.dropStatus == 'progressing') {
-        this.editTime(this.dropObj.endTimestamp)
+        this.editTime(this.dropObj.endTimestamp);
       } else {
-        this.editTime(this.dropObj.cancelTimestamp)
+        this.editTime(this.dropObj.cancelTimestamp);
       }
-      this.getSomeGoods()
+      this.getSomeGoods();
       // console.log(res)
     })
   }
@@ -111,19 +85,19 @@ export class UserShareComponent implements OnInit, OnDestroy {
       if (res === undefined) {
         return
       }
-      this.someGoodsList = [...res]
-    })
+      this.someGoodsList = [...res];
+    });
   }
   editTime(time) {
     // const tmp = 1527753479
     const self = this
     self.ngZone.runOutsideAngular(() => {
       self.timer = setInterval(() => {
-        const nowtime = new Date().getTime()
-        const mytime = new Date(time).getTime()
-        const total =  (mytime - nowtime) / 1000
+        const nowtime = new Date().getTime();
+        const mytime = new Date(time).getTime();
+        const total =  (mytime - nowtime) / 1000;
 
-        const day = Math.floor(total / ( 24 * 60 * 60))//整天
+        const day = Math.floor(total / ( 24 * 60 * 60));//整天
 
         const afterDay = total - day * 24 * 60 * 60;
         self.ahour = Math.floor(afterDay / ( 60 * 60 )); //小时
@@ -136,7 +110,7 @@ export class UserShareComponent implements OnInit, OnDestroy {
         }
         const afterMin = total - day * 24 * 60 * 60 - self.ahour * 60 * 60 - self.amin * 60;
 
-        self.asecond = Math.floor(afterMin)//秒
+        self.asecond = Math.floor(afterMin);//秒
         if ( self.asecond < 10) {
           self.asecond = '0' + self.asecond
         }
@@ -147,9 +121,9 @@ export class UserShareComponent implements OnInit, OnDestroy {
           self.ahour = '0' + self.ahour
         }
         if ((self.ahour.toString()).indexOf('-') > 0) {
-          this.ahour= '00'
-          this.amin= '00'
-          this.asecond= '00'
+          this.ahour= '00';
+          this.amin= '00';
+          this.asecond= '00';
         }
         if (!self.changeDetectorRef['destroyed']) {
           self.changeDetectorRef.detectChanges();
@@ -159,23 +133,23 @@ export class UserShareComponent implements OnInit, OnDestroy {
     // alert(time / (24 * 3600 * 1000))
   }
   openDeepLink() {
-    window.open(this.dropObj.dropLink)
+    window.open(this.dropObj.dropLink);
   }
   downApp() {
     // window.navigator
     const  sUserAgent: any = navigator.userAgent.toLowerCase();
-    this.sUserAgent = sUserAgent
+    this.sUserAgent = sUserAgent;
     const  bIsIpad = sUserAgent.match(/ipad/i) === 'ipad';
     const  bIsIphoneOs = sUserAgent.match(/iphone os/i) === 'iphone os';
     const  bIsMidp = sUserAgent.match(/midp/i) === 'midp';
-    const  bIsQQ= sUserAgent.indexOf('mqqbrowser') > -1
-    const  bIsUc =  sUserAgent.indexOf('ucbrowser') > -1
+    const  bIsQQ= sUserAgent.indexOf('mqqbrowser') > -1;
+    const  bIsUc =  sUserAgent.indexOf('ucbrowser') > -1;
     const  bIsAndroid = sUserAgent.match(/android/i) === 'android';
     if (bIsAndroid || bIsUc ||bIsQQ ) {
-      window.open('market://details?id=com.socialcommer.wx')
+      window.open('market://details?id=com.socialcommer.wx');
       // window.open('https://www.getpricedrop.com/')
     } else {
-      window.open('https://play.google.com/store/apps/details?id=com.socialcommer.wx&referrer=utm_source%3Dh5page%26utm_medium%3Dpage')
+      window.open('https://play.google.com/store/apps/details?id=com.socialcommer.wx&referrer=utm_source%3Dh5page%26utm_medium%3Dpage');
 
     }
     // if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid) {
