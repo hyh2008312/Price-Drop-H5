86cd9422fef49ebde52e3e05394a9759ec020534
zhoushini@163.com <zhoucc88>
2019-03-27 19:09:52 +0800

    count-down

--- src/app/shared/components/count-down/count-down.component.ts
+++ src/app/shared/components/count-down/count-down.component.ts
@@ -13,39 +13,13 @@ export class CountDownComponent implements AfterViewInit, OnDestroy{
   minute: any = '00';
   second: any = '00';
   _diff: number;
-  public get diff() {
-    return this._diff;
-  }
-  public set diff(val) {
-    this._diff = Math.floor(val / 1000);
-
-    this.hour = Math.floor(this._diff / 3600);
-    if ( this.hour < 10) {
-      this.hour = '0' + this.hour
-    }
-    this.minute = Math.floor((this._diff % 3600) / 60);
-    if ( this.minute < 10) {
-      this.minute = '0' + this.minute
-    }
-    this.second = (this._diff % 3600) % 60;
-    if ( this.second < 10) {
-      this.second = '0' + this.second
-    }
-    if ((this.hour.toString()).indexOf('-') > 0) {
-      this.hour = '00';
-      this.minute = '00';
-      this.second = '00';
-    }
-    console.log(this.diff)
-  }
   public timer;
 
   ngAfterViewInit() {
     // console.log( new Date(this.endDate))
-    const myDate: any = new Date(this.endDate);
     this.ngZone.runOutsideAngular(() => {
       this.timer = setInterval(() => {
-        this.diff = myDate - Date.now();
+        this.countTime()
         this.ref.detectChanges();
       }, 1000);
     });
@@ -55,7 +29,32 @@ export class CountDownComponent implements AfterViewInit, OnDestroy{
       clearInterval(this.timer);
     }
   }
+  // public get diff() {
+  //   return this._diff;
+  // }
+  countTime() {
+    const myDate: any = new Date(this.endDate);
+    let val = myDate - Date.now();
+    this._diff = Math.floor(val / 1000);
 
+    this.hour = Math.floor(this._diff / 3600);
+    if (this.hour < 10) {
+      this.hour = '0' + this.hour;
+    }
+    this.minute = Math.floor((this._diff % 3600) / 60);
+    if (this.minute < 10) {
+      this.minute = '0' + this.minute;
+    }
+    this.second = (this._diff % 3600) % 60;
+    if (this.second < 10) {
+      this.second = '0' + this.second;
+    }
+    if ((this.hour.toString()).indexOf('-') > 0) {
+      this.hour = '00';
+      this.minute = '00';
+      this.second = '00';
+    }
+  }
 
   constructor(private ref: ChangeDetectorRef, private ngZone: NgZone) {}
 }
